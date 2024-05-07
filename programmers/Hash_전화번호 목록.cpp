80d680f0e2b5d127bb212870f0cbc24c1c4f59c1
Future <86925185+allllfo@users.noreply.github.com>
2024-05-07 21:55:25 +0900

    Hash_전화번호 목록 수정

--- programmers/Hash_전화번호 목록.cpp
+++ programmers/Hash_전화번호 목록.cpp
@@ -7,13 +7,13 @@ bool solution(vector<string> phone_book) {
     bool answer = true;
     sort(phone_book.begin(), phone_book.end());
     
-    for(int i=0; i<phone_book.size(); i++){
+    for(int i=0; i<phone_book.size()-1; i++){
         int length = phone_book[i].length();
-        for(int j=i+1; j<phone_book.size(); j++){
-            if(phone_book[i] == phone_book[j].substr(0, length)){
+            if(phone_book[i] == phone_book[i+1].substr(0, length)){
                 answer = false;
+                break;
             }
-        }
     }
+    
     return answer;
 }
